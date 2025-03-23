46841fa9704e775820f8d98acd02e19cf8fb9b9d
ChatHongPT <suk924600@naver.com>
2025-03-23 12:10:35 +0900

    feat: URL 매개변수 id 값을 받아서 상품 정보 보여주는 로직

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -1,13 +1,18 @@
-import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { useProductContext } from '../contexts/ProductContext';
 
 function ProductPage(){
-    const context = useProductContext();
+    const {productId} = useParams<{productId: string}>();
+    const products = useProductContext();
+    const foundProduct = products.find((product) => product.id === parseInt(productId!, 10));
 
-    useEffect(()=>{
-        console.log(context);
-    }, [context]);
-    return <h1>상품 상세 페이지</h1>
+    return (
+        <div>
+            <h1>{foundProduct?.name}</h1>
+            <p>{foundProduct?.explanation}</p>
+            <span>{foundProduct?.price}</span>
+        </div>
+    )
 }
 
 export default ProductPage;
