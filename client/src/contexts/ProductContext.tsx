f9176a85a5e7081e8af809741c5dde71b0f998d3
ChatHongPT <suk924600@naver.com>
2025-03-23 11:55:31 +0900

    feat: 하위 컴포넌트에 데이터 전송을 위한 Provider 정의

--- client/src/contexts/ProductContext.tsx
+++ client/src/contexts/ProductContext.tsx
@@ -8,3 +8,20 @@ interface ProductType {
 }
 
 const ProductContext = createContext<ProductType[]>([]);
+
+const initialValue: ProductType[] = [
+    {
+        id: 0,
+        name: 'Iphone 13 Max',
+        explanation: '디스플레이는 6.1인치 19.5:9 비율의 2532x1170 해상도를 지원하며 패널 형식은 AMOLED 방식의 Super Retina XDR 디스플레이이다. 인치당 픽셀 수는 460 ppi이다. 120Hz의 터치 샘플링 레이트를 제공하고 명암비는 2,000,000:1이다.',
+        price: 1230000,
+      },
+];
+
+export function ProductProvider({children}: {children: React.ReactNode}){
+    return(
+        <ProductContext.Provider value = {initialValue}>
+            {children}
+        </ProductContext.Provider>
+    );
+}
